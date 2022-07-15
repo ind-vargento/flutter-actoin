b7a547f61eb0e14a7c7391f75eff32cd07f532a5
Victor Argento <vargento@sklum.com>
2022-07-15 08:30:40 +0200

    debug flutter command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sh -c "/sdks/flutter/bin/flutter build apk --no-sound-null-safety --target-platform android-arm64"
+# sh -c "/sdks/flutter/bin/flutter build apk --no-sound-null-safety --target-platform android-arm64"
+
+sh -c "/sdks/flutter/bin/flutter docker -v"
