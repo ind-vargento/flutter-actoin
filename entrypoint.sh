9508cfe599851ab0b506614ef45a89400a9b7166
Victor Argento <vargento@sklum.com>
2022-07-15 08:35:59 +0200

    back to original command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# sh -c "/sdks/flutter/bin/flutter build apk --no-sound-null-safety --target-platform android-arm64"
+sh -c "/sdks/flutter/bin/flutter build apk --no-sound-null-safety --target-platform android-arm64"
 
-sh -c "/sdks/flutter/bin/flutter doctor -v"
+# sh -c "/sdks/flutter/bin/flutter doctor -v"q
