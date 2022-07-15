e3531af1eb8edb885069ad8ec885fd42dd936fe5
Victor Argento <vargento@sklum.com>
2022-07-15 08:31:48 +0200

    fix command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # sh -c "/sdks/flutter/bin/flutter build apk --no-sound-null-safety --target-platform android-arm64"
 
-sh -c "/sdks/flutter/bin/flutter docker -v"
+sh -c "/sdks/flutter/bin/flutter doctor -v"
